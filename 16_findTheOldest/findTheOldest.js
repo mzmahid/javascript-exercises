084b8e6e9ce3627e2b98b7725c7f4739783db562
MZMahid <78899301+MZMahid@users.noreply.github.com>
2026-01-12 16:35:53 +0600

    Exercise solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(ppl) {
+    let dateObj = new Date();
+    ppl.sort((a, b) => {
+        let ageA = ((a.yearOfDeath)? a.yearOfDeath : dateObj.getFullYear()) - (a.yearOfBirth);
+        let ageB = ((b.yearOfDeath)? b.yearOfDeath : dateObj.getFullYear()) - (b.yearOfBirth);
+        return ageB - ageA;
+    });
+    return ppl[0];
 };
 
 // Do not edit below this line
